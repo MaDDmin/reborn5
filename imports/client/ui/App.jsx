9e4b82f2c5f26c692920e6a888a3f8dacadeb90e
SyBr <sybr@openmailbox.org>
2016-09-18 04:33:47 +0200

    Uns pocs canvis... enllaçant dades de formulari

--- imports/client/ui/App.jsx
+++ imports/client/ui/App.jsx
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
 
 export default class App extends Component{
+  addResolution(event){
+    event.preventDefault();
+    let text = this.refs.resolution.value.trim();
+    console.log(text);
+    this.refs.resolution.value = "";
+  }
   render(){
     return (
       <div>
         <h1>Reborn 5: POCA BROMA JA!</h1>
         <h2>My Resolutions</h2>
-        <form className="new-resolution" onSubmit="">
+        <form className="new-resolution" onSubmit={this.addResolution.bind(this)}>
           <input
             type="text"
             ref="resolution"
