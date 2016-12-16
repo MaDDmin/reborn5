fba47373c666e89992c2bd5aafa751359e20b598
SyBr <sybr@sybrcq>
2016-12-17 00:30:11 +0100

    Ja dibuixa el tauler i deixa jugar però cal encara detectar quan se guanya i viatjar a la jugada al punxar-la a l'historial.

--- imports/client/ui/components/reacteaster/TicTacToe.jsx
+++ imports/client/ui/components/reacteaster/TicTacToe.jsx
@@ -21,31 +21,42 @@ const Square = (props) =>
   ██████   ██████  ██   ██ ██   ██ ██████
   */
 class Board extends React.Component {
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)} />);
+  renderSquare(i, j){
+      return (<Square
+        key={j}
+        value={this.props.squares[i][j]}
+        onClick={() => this.props.onClick(i, j)} />);
   }
 
   render() {
+    let tauler = this.props.squares;
+
+    let result = tauler.map((linia, indexLinia)=>(
+        <div className="board-row" key={indexLinia}>
+          { linia.map((columna, indexColumna)=>(
+              this.renderSquare(indexLinia, indexColumna)
+            ))
+          }
+        </div>
+    ));
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+        {/* <div className="board-row">
+          {this.renderSquare(1, 1)}
+          {this.renderSquare(1, 2)}
+          {this.renderSquare(1, 3)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {this.renderSquare(2, 1)}
+          {this.renderSquare(2, 2)}
+          {this.renderSquare(2, 3)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+          {this.renderSquare(3, 1)}
+          {this.renderSquare(3, 2)}
+          {this.renderSquare(3, 3)}
+        </div> */}
+        {result}
       </div>
     );
   }
@@ -63,21 +74,22 @@ export default class Game extends React.Component {
     super();
     this.state = {
       history: [{
-        squares: Array(9).fill(null)
+        squares: Array(Array(3).fill(null), Array(3).fill(null), Array(3).fill(null))
       }],
       xIsNext: true,
       stepNumber: 0
     };
   }
 
-  handleClick(i) {
+  handleClick(i, j) {
     const history = this.state.history.slice(0, this.state.stepNumber+1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
+
+    if (calculateWinner(squares) || squares[i][j]) { // Si el quadre ja té un valor o s'ha guanyat la partida, no es continua amb la jugada.
       return;
     }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    squares[i][j] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       history: history.concat([{
         squares
@@ -98,19 +110,19 @@ export default class Game extends React.Component {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
-    const stepNum = this.state.stepNumber;
+    //const stepNum = this.state.stepNumber;
     const moves = history.map((step, move) => {
       const desc = move ?
         'Move #' + move :
         'Game start';
-    const classes = (move === stepNum)?"activeMove":"";
+    const classes = (move === this.state.stepNumber)?"activeMove":"";
 
       return (
         <li key={move}>
           <a
             className={classes}
             href="#"
-            onClick={(e)=>{this.jumpTo(move);}}>
+            onClick={()=>{this.jumpTo(move);}}>
             {desc}
           </a>
         </li>
@@ -130,7 +142,7 @@ export default class Game extends React.Component {
         <div className="game-board">
           <Board
             squares={current.squares}
-            onClick={(i) => this.handleClick(i)}/>
+            onClick={(i, j) => this.handleClick(i, j)}/>
         </div>
         <div className="game-info">
           <div>{status}</div>
@@ -155,22 +167,22 @@ export default class Game extends React.Component {
 ██   ██ ██    ██  ██ ██  ██ ██      ██ ██   ██ ██   ██      ██
 ██   ██  ██████  ██   ██ ██ ███████ ██ ██   ██ ██   ██ ███████
 */
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+function calculateWinner(squares) {}
+//   const lines = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6],
+//   ];
+//   for (let i = 0; i < lines.length; i++) {
+//     const [a, b, c] = lines[i];
+//     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+//       return squares[a];
+//     }
+//   }
+//   return null;
+// }
