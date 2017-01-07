b6b93f8f3e73bde1b895075675becd681a68d393
SyBr <sybr@sybrcq>
2017-01-07 13:32:57 +0100

    Bar chart hard-coded.

--- imports/client/ui/components/d3/D3Bars1.jsx
+++ imports/client/ui/components/d3/D3Bars1.jsx
@@ -5,29 +5,20 @@ export default class D3Bars1 extends Component {
   componentDidMount() {
     //console.log("Iee");
     setTimeout(()=>{
-        //console.log("Ieee222");
-        let div = d3.select(".D3Bars1"),
-          sections = div.selectAll('section'),
-          sectDiv = sections.append("div");
-
-        sectDiv.html("Ieee tots a una!!!");
+      import './D3Bars1.scss';
     }, 3000);
   }
   render() {
     return (
         <div className="D3Bars1">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <section></section>
-          <section></section>
-          <section></section>
-          <section></section>
-          <section></section>
-          <section></section>
-          <section></section>
-          <section></section>
+          <div className="chart">
+            <div style={{width: "40px"}}>4</div>
+            <div style={{width: "80px"}}>8</div>
+            <div style={{width: "150px"}}>15</div>
+            <div style={{width: "160px"}}>16</div>
+            <div style={{width: "230px"}}>23</div>
+            <div style={{width: "420px"}}>42</div>
+          </div>
         </div>
     );
   }
