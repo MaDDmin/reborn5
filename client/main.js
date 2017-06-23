a998c10906f664b3e98a42ea15638d7df787232f
SyBr <sybr@openmailbox.org>
2017-06-23 18:34:51 +0200

    Un poc de desastre...

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import '../imports/client/ui/main.scss';
 //import '../imports/client/ui/ResolutionSingle.scss';
 import '../imports/client/ui/ClientsForm.scss';
+import '../imports/client/ui/RutinesForm.scss';
 
 //import '../imports/api/collections/Resolutions.js';
 //import '../imports/api/collections/Bars.js';
