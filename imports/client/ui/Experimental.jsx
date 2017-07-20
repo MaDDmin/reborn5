dad287ce8db87c482e3de128e907541044f53da4
Pasqual <pasqg@systemli.org>
2017-07-20 15:52:03 +0200

    És humiliant...

--- imports/client/ui/Experimental.jsx
+++ imports/client/ui/Experimental.jsx
@@ -1,18 +1,79 @@
 import React, { Component } from 'react';
+import { mount } from 'react-mounter';
+import { MainLayout } from '../ui/layouts/MainLayout.jsx';
 
+function tick(){
+    const elem = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>  
+    );
+
+    mount(MainLayout, {
+        content: elem
+    });
+}
+
+setInterval(tick, 1000);
+
+export default class Experimental extends Component{
+    constructor(props){
+        super(props);
+    
+        /* this.scale = [`grey`, `red`, `blue`, `lime`, `fuchsia`, `cyan`, `gold`];
+
+        this.state = {
+            color: "grey"
+        }
+
+        this.changeColor = this.changeColor.bind(this); */
+    }
+
+    /* changeColor(){
+        return () => {
+            this.setState((prevState)=>{
+                color: `${this.scale[this.scale.indexOf(prevState.color)+1]}`
+            });
+        };
+    } */
+    
+    render(){
+        return (
+            <div>
+                <SubExperimental 
+                    ref={0} 
+                    color={`grey`} 
+                    mida={50} />
+                
+                <SubExperimental 
+                    ref={1} 
+                    color={`grey`} 
+                    mida={50} />
+
+                <SubExperimental 
+                    ref={2} 
+                    color={`grey`} 
+                    mida={50} />
+            </div>
+        )
+    }
+}
+
+// Ara definim els sub-components:
 
 class SubExperimental extends Component{
     constructor(props){
         super(props);
 
-        this.applyChangeColor = this.applyChangeColor.bind(this);
+        //this.applyChangeColor = this.applyChangeColor.bind(this);
     }
 
-    applyChangeColor(){
+    /* applyChangeColor(){
         //alert(`Sub`);
         props.changeColor;
     }
-
+ */
     render(){
         let estil = {
             border: `solid 2px black`,
@@ -27,52 +88,5 @@ class SubExperimental extends Component{
         return (
             <div style={estil} onClick={this.changeColor} />
         );
-    }
-}
-
-export default class Experimental extends Component{
-    constructor(props){
-        super(props);
-    
-        this.scale = [`grey`, `red`, `blue`, `lime`, `fuchsia`, `cyan`, `gold`];
-
-        this.state = {
-            color: "grey"
-        }
-
-        this.changeColor = this.changeColor.bind(this);
-    }
-
-    changeColor(){
-        return () => {
-            this.setState((prevState)=>{
-                color: `${this.scale[this.scale.indexOf(prevState.color)+1]}`
-            });
-        };
-    }
-
-    render(){
-        return (
-            <div>
-                <SubExperimental 
-                    ref={0} 
-                    color={this.state.color} 
-                    mida={50} 
-                    changeColor={this.changeColor} 
-                    onClick={this.changeColor()} />
-                <SubExperimental 
-                    ref={1} 
-                    color={this.state.color} 
-                    mida={50} 
-                    changeColor={this.changeColor} 
-                    onClick={this.changeColor()} />
-                <SubExperimental 
-                    ref={2} 
-                    color={this.state.color} 
-                    mida={50} 
-                    changeColor={this.changeColor} 
-                    onClick={this.changeColor()} />
-            </div>
-        )
     }
 }
