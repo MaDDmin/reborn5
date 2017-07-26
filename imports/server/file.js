17eb2a9083d9ff8a5f4287ae43fb0e1d12457ae6
Pasqual <pasqg@systemli.org>
2017-07-26 18:50:03 +0200

    Establishing final contact... Option 2 XHR + WebApp (not working)

--- imports/server/file.js
+++ imports/server/file.js
@@ -1,55 +1,17 @@
 import { Meteor } from 'meteor/meteor';
+import fs from 'fs';
 
 WebApp.connectHandlers.use('/file', (req, res) => {
-    // We only care about PUT methods
-    res.setHeader("Access-Control-Allow-Methods", "PUT");
-    // I am running meteor as a backend, see https://iamlawrence.me/agnostic-meteor
-    // Therefore we need to enable CORS
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    // Using internal WebApp
+    let 
+        start = Date.now(),
+        file = fs.createWriteStream('/public');
 
-    console.log("Zona 0");
-
-
-    // When sending a CORS request, a 'preflight' request is sent.
-    // This will determine what methods are allowed on this endpoint.
-    // Just respond happily with a 200 response
-    // See more here: https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS#Preflighted_requests
-    if (req.method === "OPTIONS") {
-
-        console.log("Zona 1");
-
+    file.on('error', () => {});
+    file.on('finish', () => {
         res.writeHead(200);
-        res.end();
-        return;
-    } else if (req.method === "PUT") {
-        // In this example, I only care about images...
-        if (!req.headers['content-type'].startsWith('image')) {
-            res.writeHead(400);
-            
-            console.log("L'encapçalament 'Content-Type' no comença per 'Image'");
-            
-            res.end();
-        }
-
-        let 
-            getFile = Meteor.wrapAsync(done => {
-                let chunks = [];
-
-                req.on('readable', () => {
-                    chunks.push(req.read());
-                    
-                    console.log("Zona 3");
-
-                });
-                req.on('end', () => {
-                    
-                    console.log("Zona 2");
-                    
-                    done(undefined, Buffer.concat(chunks));
-
-
-                });
-            });
-    }
+        res.end(); // End the response
+        
+        console.log(`Finished uploading, time taken: ${Date.now() - start}`);
+    });
 });
