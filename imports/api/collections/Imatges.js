0139ce89a7c89adcb0d76a5297c8ae3ccffa491b
SyBr <sybr@openmailbox.org>
2017-07-31 11:28:01 +0200

    Pre-Reunió

--- imports/api/collections/Imatges.js
+++ imports/api/collections/Imatges.js
@@ -4,12 +4,15 @@ import { Mongo } from 'meteor/mongo';
 export default Imatges = new Mongo.Collection('imatges');
 
 Meteor.methods({
-    'imatges.insert'(buffer) {
+    'imatges.insert'(buffer, tipusRef, idRef, ...adjunt) {
         console.log("Meteor method: 'saveImage'.");
         Imatges.insert({
             createdAt: new Date(),
             user: Meteor.userId(),
-            data: buffer
+            data: buffer,
+            tipusRef,
+            idRef,
+            ...adjunt
         });
         //console.log(`Meteor method: RIGHT AFTER Files.insert. \nBuffer: ${buffer}`);
     }
